5e1e8991ce4b4e805a4f3c20c53a2fa45c721e81
Blodzik <147334590+Blodzik@users.noreply.github.com>
2024-01-27 14:39:04 +0100

    Add files via upload

---
+++ Shop/products.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        id: 'fafjajcczo1321321',
+        name: 'Ping Pong Paddle',
+        price: '10.09',
+        image: 'ping-pong-paddle.webp'
+    },
+    {
+        id: 'fafjajcczo1321322',
+        name: 'Ball',
+        price: '20.09',
+        image: 'ping-pong-paddle.webp'
+    },
+    {
+        id: 'fafjajcczo1321323',
+        name: 'Picture',
+        price: '12.09',
+        image: 'ping-pong-paddle.webp'
+    },
+    {
+        id: 'fafjajcczo1321324',
+        name: 'Mouse',
+        price: '7.09',
+        image: 'ping-pong-paddle.webp'
+    }
+]
